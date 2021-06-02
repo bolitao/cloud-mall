110f01bbdfaef963d3df20e4a20c032a9f563f2f
Boli Tao <boli.tao@outlook.com>
2021-06-02 23:30:18 +0800

    feat: session redis sharing

--- cloud-mall-user/src/main/java/xyz/bolitao/cloudmall/user/UserApplication.java
+++ cloud-mall-user/src/main/java/xyz/bolitao/cloudmall/user/UserApplication.java
@@ -3,6 +3,7 @@ package xyz.bolitao.cloudmall.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -11,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 @MapperScan(basePackages = "xyz.bolitao.cloudmall.user.model.dao")
+@EnableRedisHttpSession
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
